d76b7c4f8b72bd120207c71b01f699883c792548
Raphaël Painter <raphapainter@wanadoo.fr>
2021-10-05 18:00:38 +0200

    fix(nada-transform): :construction: paho

--- cmd/README.md
+++ cmd/README.md
@@ -1,3 +1,5 @@
 # `/cmd`
 
 go run cmd/nada-transform/Main.go
+
+mosquitto_pub -h localhost -t test -m "blarg"
--- cmd/nada-transform/main.go
+++ cmd/nada-transform/main.go
@@ -4,12 +4,24 @@ import (
 	database "github.com/IMT-Atlantique-FIL-2020-2023/NADA-extended/internal/app/nada-transform/database"
 	myLog "github.com/IMT-Atlantique-FIL-2020-2023/NADA-extended/internal/app/nada-transform/myLog"
 	subscriber "github.com/IMT-Atlantique-FIL-2020-2023/NADA-extended/internal/app/nada-transform/subscriber"
+	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
+func onMessageReceived(client mqtt.Client, message mqtt.Message) {
+	myLog.MyLog(myLog.Get_level_INFO(), string(message.Payload()))
+
+}
+
 func main() {
+
+	var topic string = "test"
+
 	myLog.MyLog(myLog.Get_level_INFO(), "main(start)")
 	client := subscriber.Connect("tcp://localhost:1883", "my-client-id")
-	client.Publish("a/b/#", 0, false, "my great message")
+	client.Subscribe(topic, 0, onMessageReceived)
 	database.Insert()
+	for {
+
+	}
 	myLog.MyLog(myLog.Get_level_INFO(), "main(end)")
 }
