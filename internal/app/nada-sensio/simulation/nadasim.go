efd4c6ce4c48cea26a819a845f3394a58f77a47e
Alexis GROS <alexis.gros@imt-atlantique.net>
2021-10-24 21:50:29 +0200

    finished the model

--- internal/app/nada-sensio/simulation/nadasim.go
+++ internal/app/nada-sensio/simulation/nadasim.go
@@ -1,6 +1,7 @@
 package nadasensio
 
 import (
+	"fmt"
 	"math"
 	"time"
 
@@ -15,32 +16,40 @@ type SimParam struct {
 	TimeStamp        time.Time
 }
 
-var tempreature_lapse_rate float64 = 6.5
+var temperature_lapse_rate float64 = 9.8
 var reference_temperature float64 = 293.0
 
+func getNoiseFactor(params SimParam) float64 {
+	noise := perlin.NewPerlin(1, 10000, 3, int64(params.Noise_seed))
+	timerange := 100 * 365 * 24 * 3600
+	var timeseconds float64 = float64(params.TimeStamp.UnixMilli()) / 1000.0
+	x1 := float64(timeseconds)
+	x := x1 / float64(timerange)
+	noiseFactor := noise.Noise1D(x)
+	fmt.Println(noiseFactor)
+
+	return noiseFactor
+}
+
 func Pressure(params SimParam) float64 {
-	noise := perlin.NewPerlin(2, 2, 3, int64(params.Noise_seed))
+	noiseFactor := getNoiseFactor(params)
 
-	noiseFactor := noise.Noise1D(float64(params.TimeStamp.UnixMilli()) / 1000)
-
-	var reference_height float64 = 0
 	var univ_gas_const float64 = 8.314
 	var gravitational_acc float64 = 9.807
 	var molar_mass_earth_athmos float64 = 0.02896
-	var reference_pressure float64 = noiseFactor * 101300
+	var reference_pressure float64 = noiseFactor*10 + 101300
 	var height float64 = Altitude(params)
+	var reference_temperature float64 = 288
 
-	pressure := reference_pressure * math.Pow((reference_temperature+tempreature_lapse_rate*(height-reference_height)/reference_temperature), -gravitational_acc*molar_mass_earth_athmos/(univ_gas_const*tempreature_lapse_rate))
+	pressure := reference_pressure * math.Exp(-(gravitational_acc*height*molar_mass_earth_athmos)/(reference_temperature*univ_gas_const))
 
 	return pressure
 }
 
 func Altitude(params SimParam) float64 {
-	noise := perlin.NewPerlin(2, 2, 3, int64(params.Noise_seed))
+	noiseFactor := getNoiseFactor(params)
 
-	noiseFactor := noise.Noise1D(float64(params.TimeStamp.UnixMilli()) / 1000)
-
-	noiseScale := 20
+	noiseScale := 50
 
 	altitude := params.Origin_altitude + noiseFactor*float64(noiseScale)
 
@@ -48,31 +57,24 @@ func Altitude(params SimParam) float64 {
 }
 
 func Temperature(params SimParam) float64 {
-	noise := perlin.NewPerlin(2, 2, 3, int64(params.Noise_seed))
+	noiseFactor := getNoiseFactor(params)
 
-	noiseFactor := noise.Noise1D(float64(params.TimeStamp.UnixMilli()) / 1000)
-
-	temperature := Altitude(params) / 1000 * -tempreature_lapse_rate * reference_temperature * noiseFactor
-
+	var temperature float64 = reference_temperature - 273 - (Altitude(params)/1000)*temperature_lapse_rate + 5*noiseFactor
 	return temperature
 }
 
 func Latitude(params SimParam) float64 {
-	noise := perlin.NewPerlin(2, 2, 3, int64(params.Noise_seed))
+	noiseFactor := getNoiseFactor(params)
 
-	noiseFactor := noise.Noise1D(float64(params.TimeStamp.UnixMilli()) / 1000)
-
-	latitude := params.Origin_latitude * noiseFactor
+	latitude := params.Origin_latitude + 20*noiseFactor
 
 	return latitude
 }
 
 func Longitude(params SimParam) float64 {
-	noise := perlin.NewPerlin(2, 2, 3, int64(params.Noise_seed))
+	noiseFactor := getNoiseFactor(params)
 
-	noiseFactor := noise.Noise1D(float64(params.TimeStamp.UnixMilli()) / 1000)
-
-	longitude := params.Origin_longitude * noiseFactor
+	longitude := params.Origin_longitude + 20*noiseFactor
 
 	return longitude
 }
@@ -82,14 +84,13 @@ func AirPoll() {
 }
 
 func WindSpeed(params SimParam) float64 {
-	noise := perlin.NewPerlin(2, 2, 3, int64(params.Noise_seed))
-
-	noiseFactor := noise.Noise1D(float64(params.TimeStamp.UnixMilli()) / 1000)
+	noiseFactor := getNoiseFactor(params)
 
 	var windspeed_base_factor float64 = 2
 	var windspeed_increase_gradient float64 = 2
+	var altitude float64 = Altitude(params)
 
-	wind_speed := windspeed_base_factor * windspeed_increase_gradient * noiseFactor
+	wind_speed := 20 + altitude/1000*windspeed_base_factor*windspeed_increase_gradient + noiseFactor*20
 
 	return wind_speed
 }
@@ -99,9 +100,7 @@ func Humidity() {
 }
 
 func WindDirX(params SimParam) float64 {
-	noise := perlin.NewPerlin(2, 2, 3, int64(params.Noise_seed+1))
-
-	noiseFactor := noise.Noise1D(float64(params.TimeStamp.UnixMilli()) / 1000)
+	noiseFactor := getNoiseFactor(params)
 
 	wind_speed_X := 1 * noiseFactor
 
@@ -109,9 +108,7 @@ func WindDirX(params SimParam) float64 {
 }
 
 func WindDirY(params SimParam) float64 {
-	noise := perlin.NewPerlin(2, 2, 3, int64(params.Noise_seed+2))
-
-	noiseFactor := noise.Noise1D(float64(params.TimeStamp.UnixMilli()) / 1000)
+	noiseFactor := getNoiseFactor(params)
 
 	wind_speed_Y := 1 * noiseFactor
 
