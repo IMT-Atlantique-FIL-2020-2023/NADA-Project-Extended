3eb7cf0df9fbff4b40e1c8fc0c5021a1370478ed
Simon SASSI <simon.sassi@enedis.fr>
2021-10-25 20:57:38 +0000

    feat(nada-serve): implement cors configuration

--- cmd/nada-serve/main.go
+++ cmd/nada-serve/main.go
@@ -12,7 +12,8 @@ import (
 	"github.com/IMT-Atlantique-FIL-2020-2023/NADA-extended/internal/app/nada-serve/graph"
 	"github.com/IMT-Atlantique-FIL-2020-2023/NADA-extended/internal/app/nada-serve/graph/generated"
 	"github.com/IMT-Atlantique-FIL-2020-2023/NADA-extended/internal/app/nada-serve/graph/model"
-	"github.com/henvic/httpretty"
+	"github.com/go-chi/chi"
+	"github.com/rs/cors"
 	"github.com/rs/zerolog/log"
 )
 
@@ -20,6 +21,26 @@ const defaultPort = "8080"
 
 func main() {
 	config.LoadConfig()
+	router := chi.NewRouter()
+	router.Use(cors.New(cors.Options{
+		AllowedOrigins:   config.CurrentConfig.AllowedOrigins,
+		AllowCredentials: true,
+		Debug:            false,
+	}).Handler)
+	// logger := &httpretty.Logger{
+	// 	Time:           true,
+	// 	TLS:            true,
+	// 	RequestHeader:  true,
+	// 	RequestBody:    true,
+	// 	ResponseHeader: true,
+	// 	ResponseBody:   false,
+	// 	Colors:         true, // erase line if you don't like colors
+	// 	Formatters:     []httpretty.Formatter{&httpretty.JSONFormatter{}},
+	// }
+	// if config.CurrentConfig.LogLevel == zerolog.DebugLevel {
+	// 	//router.Use(logger.Middleware)
+	// }
+
 	port := fmt.Sprint(config.CurrentConfig.Port)
 
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: graph.NewResolver(), Directives: generated.DirectiveRoot{
@@ -34,21 +55,9 @@ func main() {
 		},
 	}}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
-
-	logger := &httpretty.Logger{
-		Time:           true,
-		TLS:            true,
-		RequestHeader:  true,
-		RequestBody:    true,
-		ResponseHeader: true,
-		ResponseBody:   true,
-		Colors:         true, // erase line if you don't like colors
-		Formatters:     []httpretty.Formatter{&httpretty.JSONFormatter{}},
-	}
-	logger.Middleware(srv)
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", srv)
 
 	log.Info().Msgf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal().Err((http.ListenAndServe(":"+port, nil)))
+	log.Fatal().Err((http.ListenAndServe(":"+port, router)))
 }
